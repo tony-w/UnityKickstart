29a9e4788b532573f9d5d94f06bbf85bf1604877
Karl Gluck <karlgluck@gmail.com>
2014-10-21 18:00:49 -0500

    Starting touch project

---
+++ jollytouch/Assets/Editor/JollyDebugInspector.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using System.Collections;
+using UnityEngine;
+using Jolly;
+
+[CustomEditor(typeof(JollyDebug))] 
+public class JollyDebugInspector : Editor {
+	
+	public override void OnInspectorGUI()
+	{
+		JollyDebug jollyDebug = (JollyDebug)this.target;
+
+		this.DisplayExpressions(jollyDebug);
+
+		base.OnInspectorGUI();
+	}
+
+	private void DisplayExpressions (JollyDebug jollyDebug)
+	{
+		IEnumerator enumerator = jollyDebug.ExpressionsByOwnerEnumerator;
+		while (enumerator.MoveNext())
+		{
+			JollyDebug.ExpressionsByOwner expressionsByOwner = (JollyDebug.ExpressionsByOwner)enumerator.Current;
+			expressionsByOwner.Enabled = EditorGUILayout.InspectorTitlebar(expressionsByOwner.Enabled, expressionsByOwner.Owner);
+			if (expressionsByOwner.Enabled)
+			{
+				foreach (JollyDebug.Expression expression in expressionsByOwner.Expressions)
+				{
+					EditorGUILayout.LabelField(expression.Name, expression.LastValue);
+				}
+			}
+		}
+	}
+	
+	public override bool RequiresConstantRepaint ()
+	{
+		return true;
+	}
+}
