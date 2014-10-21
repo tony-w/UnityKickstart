0ed5df5cb4fc76222aedb55591406152cad6ffd3
Karl Gluck <karlgluck@gmail.com>
2014-10-21 18:00:16 -0500

    Files to include in every project

---
+++ template/Assets/Scripts/Vector3.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Jolly
+{
+	public static class Vector3Ext
+	{
+		public static Vector3 SetX(this Vector3 self, float value)
+		{
+			return new Vector3(value, self.y, self.z);
+		}
+
+		public static Vector3 SetY(this Vector3 self, float value)
+		{
+			return new Vector3(self.x, value, self.z);
+		}
+			                   
+		public static Vector3 SetZ(this Vector3 self, float value)
+		{
+			return new Vector3(self.x, self.y, value);
+		}
+
+		public static Vector3 SetXY(this Vector3 self, float xValue, float yValue)
+		{
+			return new Vector3(xValue, yValue, self.z);
+		}
+	}
+}
+
