601464272898e664336004044f307ed490a67b23
Karl Gluck <karlgluck@gmail.com>
2014-10-21 18:59:49 -0500

    Some basics of the game

---
+++ jollytouch/Assets/Scripts/Satellite.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Jolly;
+
+public class Satellite : MonoBehaviour {
+
+	public float GravityForce;
+
+	void Start ()
+	{
+	
+	}
+	
+	void FixedUpdate ()
+	{
+		foreach (GameObject go in GameObject.FindGameObjectsWithTag("GravitySource"))
+		{
+			Vector3 delta = (go.transform.position - this.transform.position);
+			this.rigidbody2D.AddForce(delta.normalized.xy () * this.rigidbody2D.mass * go.rigidbody2D.mass / delta.magnitude);
+		}
+	}
+}
---
+++ jollytouch/Assets/Scripts/Sun.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sun : MonoBehaviour {
+
+	void Start ()
+	{
+	
+	}
+
+	void OnPreRender ()
+	{
+	}
+
+	void Update ()
+	{
+		this.transform.position = Vector3.zero;
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		GameObject.Destroy(collision.gameObject);
+	}
+}
