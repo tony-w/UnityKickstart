0369ae27f4b20ff4c47f150a1c173d843f0ca7be
Andy Wickham <awickham@cs.utexas.edu>
2014-11-16 20:06:41 -0600

    Forgot to save scene before committing.

--- Endocron/Assets/Scripts/FollowPlayers.cs
+++ Endocron/Assets/Scripts/FollowPlayers.cs
@@ -11,7 +11,8 @@ public class FollowPlayers : MonoBehaviour
 		float height = Player.transform.localScale.y * 1.8f;
 		Quaternion direction = Player.transform.rotation;
 		Vector3 offsetDirection = new Vector3 (Mathf. Sin (direction.y * Mathf.PI / 180.0f), 0, Mathf.Cos (direction.y * Mathf.PI / 180.0f));
-		this.camera.transform.position = Player.transform.position + new Vector3(0.0f, height, 0.0f) + offsetDirection * 2.0f;
+		this.camera.transform.position = Player.transform.position + new Vector3 (0.0f, height, 0.0f);
+//		this.camera.transform.Translate (new Vector3(),);
 		this.camera.transform.rotation = Player.transform.rotation;
 	}
 
